19810f6226adb58a6c9d06ba1a7b371ceef9aab0
ValenciaJCamilo <camilovalncias@gmail.com>
2022-01-10 11:07:13 -0500

    Condicionales

---
+++ Curso Básico de Python/Ejercicios/condicionales.py
@@ -0,0 +1,23 @@
+# PRIMER EJEMPLO DE CLASE
+# edad = int(input('Escribe tu edad: '))
+
+# if edad > 17:
+#     print('Eres mayor de edad')
+# else:
+#     print('Eres menor de edad')
+
+
+# Decimos al usuario que ingrese un número
+numero = int(input('Escribe un número: '))
+# Si el número es mayor a 5
+if numero > 5:
+    # Entonces imprime esto
+    print('Es mayor a 5')
+# Si el número es 5 
+elif numero == 5:
+    # Entonces imprime esto
+    print('Es igual a 5')
+# De lo contrario
+else:
+    # Imprime esto
+    print('Es menor a 5')
