ca947dc0f741ba3cb5e61d1e9ef081ad75cd92e5
ValenciaJCamilo <camilovalncias@gmail.com>
2022-01-14 17:07:50 -0500

    Sección de planes. Se agrega scroll horizontal también en comisiones. Se añaden nuevos svg para los colores de las flechas

---
+++ Curso de Responsive Design - Maquetación Mobile First/Batatabit/js/index.js
@@ -0,0 +1,35 @@
+window.sr=ScrollReveal();
+
+
+    sr.reveal('.header', {
+        duration:1900,
+        origin:'bottom',
+        distance:'-100px'
+    });
+
+    sr.reveal('.logo-batatabit', {
+        delay:700,
+        rotate:{
+            x:1,
+            y:180,
+        }
+    });
+
+    sr.reveal('.main-exchange-container', {
+        duration:2500
+    });
+
+    sr.reveal('.product-detail--card', {
+        duration:1500,
+        origin:'left',
+        distance:'100px'
+    });
+
+    sr.reveal('.bitcoin-img-container', {
+        duration:1500
+    });
+
+    sr.reveal('.main-plans-container', {
+        duration:1500
+    });
+    
